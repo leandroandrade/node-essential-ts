c1698c4d150570b6916bb84248cde2c85604c1fa
leandroandrade <leandro.andrade2588@gmail.com>
2021-02-18 19:48:28 -0300

    eslint

--- src/configurations/app/index.ts
+++ src/configurations/app/index.ts
@@ -2,8 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.json({message: 'Hello World'})
-})
+app.get('/', (req, res) => res.json({ message: 'Hello World' }));
 
 export default app;

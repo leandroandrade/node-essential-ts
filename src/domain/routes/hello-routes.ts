832eaa1adf34da282af6a9484df259db6966afe9
leandroandrade <leandro.andrade2588@gmail.com>
2021-03-02 09:23:40 -0300

    fix controllers

--- src/domain/routes/hello-routes.ts
+++ src/domain/routes/hello-routes.ts
@@ -2,5 +2,5 @@ import { Router } from 'express';
 import HelloController from '../../entrypoints/rest/hello-controller';
 
 export default (router: Router): void => {
-    router.get('/hello', HelloController);
+    router.get('/hello', HelloController.getHello);
 };
--- src/entrypoints/rest/hello-controller.ts
+++ src/entrypoints/rest/hello-controller.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from 'express';
 import HelloRepository from '../../dataproviders/repositories/hello-repository';
 
-export default async (req: Request, res: Response, next) => {
+const getHello = async (req: Request, res: Response): Promise<Response> => {
     const message = await HelloRepository.getMessage();
     return res.json({
         title: 'hello',
         message,
     });
+};
+
+export default {
+    getHello,
 };
